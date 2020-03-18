1dfd89119637515808a6077f651632b74ebd8707
Maher Attouche <maherattouche7@gmail.com>
2020-03-18 11:52:09 +0100

    Tests Junit constructeur statique

--- src/test/java/uvsq/pglp_3_3/TestRobot.java
+++ src/test/java/uvsq/pglp_3_3/TestRobot.java
@@ -9,8 +9,14 @@ public class TestRobot {
 	@Test
 	public void testConstructeurDynamique() {
 			RobotDynamique rb=new RobotDynamique(1,5);
-			assert(rb.orientation==1 && rb.Position==5);
-		
+			assert(rb.orientation==1 && rb.Position==5);		
 	}
+	
+	@Test
+	public void testConstructeurStatique() {
+			RobotStatique rb=new RobotStatique(1,5);
+			assert(rb.orientation==1 && rb.Position==5);		
+	}
+	
 
 }
